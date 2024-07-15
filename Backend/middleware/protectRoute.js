204830a6a3cf35c3c2e47b3c146138a41ca3b31a
jayesh70599 <jayesh70599@gmail.com>
2024-07-15 17:11:33 +0530

    Prepared for deployment

--- Backend/middleware/protectRoute.js
+++ Backend/middleware/protectRoute.js
@@ -4,11 +4,14 @@ import User from "../models/user.model.js";
 export const protectRoute = async (req, res, next) => {
     try {
         const token = req.cookies.jwt;
+
         if(!token){
             return res.status(401).json({error: "Unauthorised: No Token Provided"});
         }
 
+
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
 
         if(!decoded){
             return res.status(401).json({error: "Unauthorised: Invalid Token"});
--- Frontend/src/pages/auth/signup/SignUpPage.jsx
+++ Frontend/src/pages/auth/signup/SignUpPage.jsx
@@ -7,7 +7,7 @@ import { MdOutlineMail } from "react-icons/md";
 import { FaUser } from "react-icons/fa";
 import { MdPassword } from "react-icons/md";
 import { MdDriveFileRenameOutline } from "react-icons/md";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 
 const SignUpPage = () => {
@@ -17,6 +17,8 @@ const SignUpPage = () => {
 		fullname: "",
 		password: "",
 	});
+
+	const queryClient = useQueryClient();
 
     const {mutate, isError, isPending, error} = useMutation({
         mutationFn: async({email, username, fullname, password}) => {
@@ -41,6 +43,7 @@ const SignUpPage = () => {
         },
         onSuccess: () =>{
             toast.success("Account created successfully");
+			queryClient.invalidateQueries({ queryKey: ["authUser"] });
         }
     });
 
